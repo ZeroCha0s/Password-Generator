ad7ad412552cf196d3d1491c69b3bdb6ebb8c85f
Devon <84931772+ZeroCha0s@users.noreply.github.com>
2023-02-13 09:46:50 -0500

    Add files via upload

---
+++ mypassword_generator.py
@@ -0,0 +1,19 @@
+import random
+
+print("Welcome to Password Generator")
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$*&().,'
+
+number = input('Amount of passwords generated: ')
+number = int(number)
+
+length = input('Input your password within a certain length: ')
+length = int(length)
+
+print('\nThe following are generated passwords: ')
+
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
